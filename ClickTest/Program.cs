4a54c9cb79e87cb526e1ce1ddeb1502b7ec69dda
ShTair <git@vsh.jp>
2019-07-28 17:15:46 +0900

    プロジェクト ファイルを追加します。

---
+++ ClickTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ClickTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.Run(RunAsync);
+            Console.ReadLine();
+        }
+
+        private static async Task RunAsync()
+        {
+            while (true)
+            {
+                Mouse.Click(-250, 880);
+                await Task.Delay(2000);
+
+                Mouse.Click(-350, 860);
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
